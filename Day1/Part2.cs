7cb81cb91e2478402001bdeaa4815b8aa252b65f
SamBobBarnes <sbarnes@vermeer.com>
2025-12-01 10:58:45 -0600

    d1p2

--- Day1/Part2.cs
+++ Day1/Part2.cs
@@ -1,12 +1,35 @@
 namespace AOC25.Day1;
 
-public class Part2() : BasePart(1,2,true)
+public class Part2() : BasePart(1,2)
 {
     public override string Run()
     {
-        //var input = Input();
-        //var input = InputChars();
+        var input = Input().Select(x => (Direction: x.Substring(0, 1), Distance: int.Parse(x.Substring(1)))).ToList();
 
-        return 0.ToString();
+        var currentPosition = 50;
+        var limit = 100;
+        var total = 0;
+
+        foreach (var rotation in input)
+        {
+            if(rotation.Direction == "R")
+            {
+                currentPosition += rotation.Distance;
+                total += currentPosition/limit;
+                currentPosition %= limit;
+            }
+            else
+            {
+                if(currentPosition != 0)
+                    currentPosition = limit - currentPosition;
+                currentPosition += rotation.Distance;
+                total += currentPosition/limit;
+                currentPosition %= limit;
+                if(currentPosition != 0)
+                    currentPosition = limit - currentPosition;
+            }
+        }
+
+        return total.ToString();
     }
 }
