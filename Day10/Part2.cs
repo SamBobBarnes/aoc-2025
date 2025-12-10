4276b08deb9198658dbeb786eb4fcd3cea82dba7
SamBobBarnes <sbarnes@vermeer.com>
2025-12-10 15:13:01 -0600

    wip solver

--- Day10/Part2.cs
+++ Day10/Part2.cs
@@ -1,6 +1,8 @@
+using Google.OrTools.LinearSolver;
+
 namespace AOC25.Day10;
 
-public class Part2() : BasePart(10,2,true)
+public class Part2() : BasePart(10,2)
 {
     public override string Run()
     {
@@ -10,60 +12,78 @@ public class Part2() : BasePart(10,2,true)
         foreach(var machine in input)
         {
             var state = new State($"({string.Join(",", Enumerable.Repeat(0, machine.GoalState.Length))})");
-            var presses = GetButtonPresses(state, machine, 0, new int[machine.ButtonWirings.Count]);
+            var solver = Solver.CreateSolver("SCIP");
+            var x0 = solver.MakeIntVar(0.0, double.PositiveInfinity, "x0");
+            var x1 = solver.MakeIntVar(0.0, double.PositiveInfinity, "x1");
+            var x2 = solver.MakeIntVar(0.0, double.PositiveInfinity, "x2");
+            var x3 = solver.MakeIntVar(0.0, double.PositiveInfinity, "x3");
+            var x4 = solver.MakeIntVar(0.0, double.PositiveInfinity, "x4");
+            var x5 = solver.MakeIntVar(0.0, double.PositiveInfinity, "x5");
+            var x6 = solver.MakeIntVar(0.0, double.PositiveInfinity, "x6");
+            var x7 = solver.MakeIntVar(0.0, double.PositiveInfinity, "x7");
+            var x8 = solver.MakeIntVar(0.0, double.PositiveInfinity, "x8");
+            var x9 = solver.MakeIntVar(0.0, double.PositiveInfinity, "x9");
 
-            total += presses.Sum();
+            for(int i = 0; i < machine.GoalState.Length; i++)
+            {
+                var c = machine.ButtonWirings.Count;
+                var w0 = c > 0 ? machine.ButtonWirings[0].Wiring.Contains(i) ? 1 : 0 : 0;
+                var w1 = c > 1 ? machine.ButtonWirings[1].Wiring.Contains(i) ? 1 : 0 : 0;
+                var w2 = c > 2 ? machine.ButtonWirings[2].Wiring.Contains(i) ? 1 : 0 : 0;
+                var w3 = c > 3 ? machine.ButtonWirings[3].Wiring.Contains(i) ? 1 : 0 : 0;
+                var w4 = c > 4 ? machine.ButtonWirings[4].Wiring.Contains(i) ? 1 : 0 : 0;
+                var w5 = c > 5 ? machine.ButtonWirings[5].Wiring.Contains(i) ? 1 : 0 : 0;
+                var w6 = c > 6 ? machine.ButtonWirings[6].Wiring.Contains(i) ? 1 : 0 : 0;
+                var w7 = c > 7 ? machine.ButtonWirings[7].Wiring.Contains(i) ? 1 : 0 : 0;
+                var w8 = c > 8 ? machine.ButtonWirings[8].Wiring.Contains(i) ? 1 : 0 : 0;
+                var w9 = c > 9 ? machine.ButtonWirings[9].Wiring.Contains(i) ? 1 : 0 : 0;
+
+
+                solver.Add(
+                        x0 * w0 +
+                           x1 * w1 +
+                           x2 * w2 +
+                           x3 * w3 +
+                           x4 * w4 +
+                           x5 * w5 +
+                           x6 * w6 +
+                           x7 * w7 +
+                           x8 * w8 +
+                           x9 * w9
+                           == machine.GoalState.Wiring[i]
+                        );
+            }
+
+            solver.Minimize(x0 + x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9);
+            solver.Solve();
+            var objective = solver.Objective();
+            var x0Val = x0.SolutionValue();
+            var x1Val = x1.SolutionValue();
+            var x2Val = x2.SolutionValue();
+            var x3Val = x3.SolutionValue();
+            var x4Val = x4.SolutionValue();
+            var x5Val = x5.SolutionValue();
+            var x6Val = x6.SolutionValue();
+            var x7Val = x7.SolutionValue();
+            var x8Val = x8.SolutionValue();
+            var x9Val = x9.SolutionValue();
+            total += (int)objective.Value();
         }
 
 
         return total.ToString();
     }
 
-    private int[] GetButtonPresses(State state, Machine machine, int buttonIndex, int[] presses)
-    {
-        presses[buttonIndex] = 0;
-        while(!state.ExceedsGoal(machine.GoalState))
-        {
-             state += machine.ButtonWirings[buttonIndex];
-             presses[buttonIndex]++;
-        }
-
-        state -= machine.ButtonWirings[buttonIndex];
-        presses[buttonIndex]--;
-
-        if (buttonIndex == machine.ButtonWirings.Count - 1) // last button
-        {
-            if (state.Equals(machine.GoalState)) return presses;
-            presses[buttonIndex] = -1; // go back up
-            return presses;
-        }
-
-        var result = GetButtonPresses(state, machine, buttonIndex + 1, presses);
-        while(result[buttonIndex + 1] == -1 && result[buttonIndex] > 0)
-        {
-            state -= machine.ButtonWirings[buttonIndex];
-            presses[buttonIndex]--;
-            result = GetButtonPresses(state, machine, buttonIndex + 1, presses);
-        }
-
-        if(result[buttonIndex + 1] == -1)
-        {
-            presses[buttonIndex] = -1;
-        }
-
-        return presses;
-    }
-
     private class State(string config)
     {
-        private readonly int[] _wiring = config.TrimStart('(').TrimEnd(')').Split(',').Select(int.Parse).ToArray();
+        public readonly int[] Wiring = config.TrimStart('(').TrimEnd(')').Split(',').Select(int.Parse).ToArray();
 
-        public int Length => _wiring.Length;
+        public int Length => Wiring.Length;
         public static State operator +(State a, State b)
         {
-            for (int i = 0; i < b._wiring.Length; i++)
+            for (int i = 0; i < b.Wiring.Length; i++)
             {
-                a._wiring[b._wiring[i]]++;
+                a.Wiring[b.Wiring[i]]++;
             }
 
             return a;
@@ -71,9 +91,9 @@ public class Part2() : BasePart(10,2,true)
 
         public static State operator -(State a, State b)
         {
-            for (int i = 0; i < b._wiring.Length; i++)
+            for (int i = 0; i < b.Wiring.Length; i++)
             {
-                a._wiring[b._wiring[i]]--;
+                a.Wiring[b.Wiring[i]]--;
             }
 
             return a;
@@ -81,23 +101,23 @@ public class Part2() : BasePart(10,2,true)
 
         public override string ToString()
         {
-            return string.Join(",", _wiring);
+            return string.Join(",", Wiring);
         }
 
         public bool ExceedsGoal(State goal)
         {
-            for (int i = 0; i < _wiring.Length; i++)
+            for (int i = 0; i < Wiring.Length; i++)
             {
-                if (_wiring[i] > goal._wiring[i]) return true;
+                if (Wiring[i] > goal.Wiring[i]) return true;
             }
             return false;
         }
 
         public bool Equals(State other)
         {
-            for (int i = 0; i < _wiring.Length; i++)
+            for (int i = 0; i < Wiring.Length; i++)
             {
-                if (_wiring[i] != other._wiring[i]) return false;
+                if (Wiring[i] != other.Wiring[i]) return false;
             }
             return true;
         }
